ff0185413b385d99f27d838640aeef74e6fee012
Malka <malka.nessy@gmail.com>
2017-06-01 18:38:37 +0300

    attention_show

--- js/app.js
+++ js/app.js
@@ -1,36 +1,51 @@
 
 	function timedText() {
-    setTimeout(myTimeout1, 2000) 
-     
+    //setTimeout(myTimeout1, 2000) 
+    setTimeout(myTimeoutAttention, 3000)  
 	}
-	function myTimeout1() {
+	
+	/* function myTimeout1() {
 		document.getElementById("demo").innerHTML = "2 seconds";
+	} */
+	
+	function myTimeoutAttention() {
+		$( "#button" ).hide();
+		document.getElementById("demo").innerHTML = "надпись исчезла через 3 seconds";
 	}
+	 
+	function attention_show (message) 
+	{
+		put_button_text (message);
+		$( "#button" ).show();
+		timedText();
+	}
+	
+	function put_button_text (message)
+	{
+		$( "#button" ).text(message);
+	}
+	
 	
 	 function click_about_show () //?? требуется дабл-клик вместо клика
 	{
 		$("#about").click(function(){
+			about_show ();
 			$( this ).toggleClass("selected");
-			$("#page #p_about").toggle();
-			 
-		});
-						
+		});			
 	} 
 	
 	 function click_todo_show () //?? требуется дабл-клик вместо клика
 	{
 		$("#todo").click(function(){
-			$("#page #p_todo").toggle();
+			todo_show ();
 			$( this ).toggleClass("selected"); 
-		});
-						
+		});			
 	} 
 	
 	
 	function about_show ()
 	{
 		$("#page #p_about").toggle();
-		
 	}
 	
 	function todo_show ()
@@ -42,28 +57,16 @@ $(document).ready(function()
 {
 	$("#p_about").hide();
 	$("#p_todo").hide();
-	$(".button").hide();
+	$("#button").hide();
 	
-	function button_hide ()
+	/* function button_hide ()
 	{
 		$( "#big-button" ).hide();
-	}
+	} 	
 	
-	function put_button_text (message)
-	{
-		$( "#big-button" ).text(message);
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	$( "ul li" ).click(function() {
-			/* button_hide (); */
+	 $( "ul li" ).click(function() {
+			button_hide (); 
 		put_button_text ("gfgfgfg");
-	});
-
+	});*/
+ 
 });
